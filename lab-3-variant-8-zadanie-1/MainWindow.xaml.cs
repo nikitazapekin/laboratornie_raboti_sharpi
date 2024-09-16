5dcc3f9e457eef1c297d443779e879880b88ac1a
nikitazapekin <wotblitz362@mail.ru>
2024-09-16 09:39:28 +0300

    min and sum

--- lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
+++ lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Text;
 using System.Windows;
 
@@ -6,27 +7,137 @@ namespace lab_3_variant_8_zadanie_1
 {
     public partial class MainWindow : Window
     {
+
+        private int[] randomNumbers;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+   
+        private int GetMinimalElement()
+        {
+            if (randomNumbers == null || randomNumbers.Length == 0)
+                return 0;
+
+            int min = randomNumbers[0];
+            foreach (var num in randomNumbers)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            return min;
+        }
+
+        private int GetSumBetweenFirstTwoNegatives()
+        {
+            if (randomNumbers == null || randomNumbers.Length == 0)
+                return 0;
+
+            int firstNegIndex = -1, secondNegIndex = -1;
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                if (randomNumbers[i] < 0)
+                {
+                    if (firstNegIndex == -1)
+                    {
+                        firstNegIndex = i;
+                    }
+                    else
+                    {
+                        secondNegIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            if (firstNegIndex == -1 || secondNegIndex == -1)
+            {
+                return 0;
+            }
+
+
+            int sum = 0;
+            for (int i = firstNegIndex + 1; i < secondNegIndex; i++)
+            {
+                sum += randomNumbers[i];
+            }
+            return sum;
+        }
+
+  
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-            // Проверка, что введено корректное число n
+            if (int.TryParse(InputN.Text, out int n) && n > 0)
+            {
+                Random random = new Random();
+                randomNumbers = new int[n]; 
+
+                StringBuilder sb = new StringBuilder();
+
+              
+                for (int i = 0; i < n; i++)
+                {
+                    randomNumbers[i] = random.Next(-20, 101);  
+                    sb.Append(randomNumbers[i]);
+                    sb.Append(" ");
+                }
+
+              
+                OutputNumbers.Text = sb.ToString();
+ 
+                int minElement = GetMinimalElement();
+                MinNumber.Text = minElement.ToString();
+
+              
+                int sumBetweenNegatives = GetSumBetweenFirstTwoNegatives();
+                Sum.Text = sumBetweenNegatives.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное положительное число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
+
+/*
+using System;
+using System.Text;
+using System.Windows;
+
+namespace lab_3_variant_8_zadanie_1
+{
+    public partial class MainWindow : Window
+    {
+        private int minElement;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int GetMinimalElement()
+        {
+            return 0;
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            
             if (int.TryParse(InputN.Text, out int n) && n > 0)
             {
                 Random random = new Random();
                 StringBuilder sb = new StringBuilder();
 
-                // Генерация n случайных чисел
+              
                 for (int i = 0; i < n; i++)
                 {
-                    sb.Append(random.Next(1, 101)-20); // Генерация числа от 1 до 100
+                    sb.Append(random.Next(1, 101)-20);  
                     sb.Append(" ");
                 }
-
-                // Вывод чисел
+ 
                 OutputNumbers.Text = sb.ToString();
             }
             else
@@ -37,7 +148,7 @@ namespace lab_3_variant_8_zadanie_1
     }
 }
 
-
+*/
 
 /*
  * using System;
