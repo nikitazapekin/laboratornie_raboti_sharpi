36be37e1112c17e3b5c9b848c3d3927871b85879
nikitazapekin <wotblitz362@mail.ru>
2024-09-16 09:20:56 +0300

    random generation

--- lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
+++ lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
@@ -1,4 +1,46 @@
 ﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace lab_3_variant_8_zadanie_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка, что введено корректное число n
+            if (int.TryParse(InputN.Text, out int n) && n > 0)
+            {
+                Random random = new Random();
+                StringBuilder sb = new StringBuilder();
+
+                // Генерация n случайных чисел
+                for (int i = 0; i < n; i++)
+                {
+                    sb.Append(random.Next(1, 101)-20); // Генерация числа от 1 до 100
+                    sb.Append(" ");
+                }
+
+                // Вывод чисел
+                OutputNumbers.Text = sb.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное положительное число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
+
+
+
+/*
+ * using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,3 +68,4 @@ namespace lab_3_variant_8_zadanie_1
         }
     }
 }
+*/
