68d2d03f4dece0d068d03b21ddf6e7e33c0ae6e4
nikitazapekin <wotblitz362@mail.ru>
2024-09-20 12:11:12 +0300

    fix issues

--- lab-2.1-zadanie1-variant8/MainWindow.xaml.cs
+++ lab-2.1-zadanie1-variant8/MainWindow.xaml.cs
@@ -66,16 +66,16 @@ namespace lab_2._1_zadanie1_variant8
                 else if (i >= -6 && i <= 2)
                 {
                     double f = (0.5) * i + 1;
-                 //   output.AppendLine($"{i,7:F2} 0");
+                
                      output.AppendLine($"{i,7:F2} {f,10:F5}");
                     amountOfElements += 1;
                 }
                 else if (i >= 2 && i <= 6)
                 {
-                    // Add the function value for interval [2, 6]
-                    double f = Math.Pow(i - 4, 2) + 2; // Example function for [2, 6]
-                                                       //   output.AppendLine($"{i,7:F2} {f,10:F5}");
-                                                       //     output.AppendLine($"{i,7:F2} 0");
+                   
+                    double f = Math.Pow(i - 4, 2) + 2; 
+                                                      
+                                                       
                     output.AppendLine($"{i,7:F2}  {0,10:F5}");
                     amountOfElements += 1;
                 }
@@ -84,6 +84,10 @@ namespace lab_2._1_zadanie1_variant8
                     double f = Math.Pow(i - 6, 2);
                     output.AppendLine($"{i,7:F2} {f,10:F5}");
                     amountOfElements += 1;
+                }
+                else if (i<-10 || i>8)
+                {
+                    output.AppendLine($"{i,7:F2} Значение находится вне диапазона");
                 }
             }
 
@@ -94,58 +98,6 @@ namespace lab_2._1_zadanie1_variant8
 
             OutputTextBlock.Text = output.ToString();
         }
-
-        /*
-        private void OnCalculateClick(object sender, RoutedEventArgs e)
-        {
-
-            if (!double.TryParse(XStartTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double xstart) ||
-                !double.TryParse(XEndTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double xend) ||
-                !double.TryParse(DxTextBox.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out double dx))
-            {
-                MessageBox.Show("Please enter valid numbers.");
-                return;
-            }
-            if (xstart >= xend)
-            {
-                MessageBox.Show("Пожалуйста введите корректные промежутки.");
-                return;
-            }
-
-            StringBuilder output = new StringBuilder();
-            output.AppendLine("    x      f(x)");
-
-            for (double i = xstart; i <= xend; i += dx)
-            {
-                if (i >= -10 && i <= -6)
-                {
-                    double f = Math.Sqrt(4 - Math.Pow(i + 8, 2)) - 2;
-                    output.AppendLine($"{i} {f:F5}");
-                    amountOfElements += 1;
-                }
-                else if (i >= -6 && i <= 2)
-                {
-                    double f = (0.5) * i + 1;
-                    output.AppendLine($"{i} {f:F5}");
-                    amountOfElements += 1;
-                }
-                else if (i >= 6 && i <= 8)
-                {
-                    double f = Math.Pow(i - 6, 2);
-                    amountOfElements += 1;
-                    output.AppendLine($"{i} {f:F5}");
-                } 
-            }
-                if(amountOfElements == 0)
-                {
-                    output.AppendLine("Элементы отсутствуют");
-
-                }
-
-            OutputTextBlock.Text = output.ToString();
-        }
-
-        */
 
 
 
