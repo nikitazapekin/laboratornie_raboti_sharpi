19ebed14a9a9c5650c3ce7bd15d70850e74a2e6a
nikitazapekin <wotblitz362@mail.ru>
2024-10-02 19:37:58 +0300

    fix editing

--- lab-3-variant-8-zadanie-1/InputDialog.xaml.cs
+++ lab-3-variant-8-zadanie-1/InputDialog.xaml.cs
@@ -27,19 +27,4 @@ namespace lab_3_variant_8_zadanie_1
         }
     }
 }
-
-
-/*
- * using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lab_3_variant_8_zadanie_1
-{
-    internal class Class1
-    {
-    }
-}
-*/
+ 
--- lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
+++ lab-3-variant-8-zadanie-1/MainWindow.xaml.cs
@@ -1,4 +1,115 @@
 ﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab_3_variant_8_zadanie_1
+{
+    public partial class MainWindow : Window
+    {
+        private int[] randomNumbers;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InputButton_Click(object sender, RoutedEventArgs e)
+        {
+            InputDialog inputDialog = new InputDialog();
+            if (inputDialog.ShowDialog() == true)
+            {
+                randomNumbers = inputDialog.Numbers;
+                FillDataGrid(randomNumbers, DataGridView);
+                UpdateValues();
+            }
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(InputN.Text, out int n) && n > 0)
+            {
+                Random random = new Random();
+                randomNumbers = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    randomNumbers[i] = random.Next(-20, 101);
+                }
+
+                FillDataGrid(randomNumbers, DataGridView);
+                UpdateValues();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное положительное число.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void FillDataGrid(int[] array, DataGrid dataGrid)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Число", typeof(int));
+            foreach (var num in array)
+            {
+                dt.Rows.Add(num);
+            }
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+      
+        private void DataGridView_CurrentCellChanged(object sender, EventArgs e)
+        {
+ 
+            DataView dv = (DataView)DataGridView.ItemsSource;
+            randomNumbers = dv.Table.Rows.Cast<DataRow>().Select(row => Convert.ToInt32(row["Число"])).ToArray();
+
+          
+            UpdateValues();
+        }
+
+        private void UpdateValues()
+        {
+        
+            int minElement = GetMinimalElement();
+            MinNumber.Text = minElement.ToString();
+
+           
+            int sumBetweenNegatives = GetSumBetweenFirstTwoNegatives();
+            Sum.Text = sumBetweenNegatives.ToString();
+
+           
+            int[] transformedArray = TransformArray();
+            FillDataGrid(transformedArray, TransformedDataGridView);
+        }
+
+        private int GetMinimalElement()
+        {
+            return randomNumbers.Min();
+        }
+
+        private int GetSumBetweenFirstTwoNegatives()
+        {
+            int firstNegIndex = Array.FindIndex(randomNumbers, num => num < 0);
+            int secondNegIndex = Array.FindIndex(randomNumbers, firstNegIndex + 1, num => num < 0);
+
+            if (firstNegIndex == -1 || secondNegIndex == -1)
+                return 0;
+
+            return randomNumbers.Skip(firstNegIndex + 1).Take(secondNegIndex - firstNegIndex - 1).Sum();
+        }
+
+        private int[] TransformArray()
+        {
+            int[] lessThanOne = Array.FindAll(randomNumbers, num => Math.Abs(num) <= 1);
+            int[] greaterThanOne = Array.FindAll(randomNumbers, num => Math.Abs(num) > 1);
+            return lessThanOne.Concat(greaterThanOne).ToArray();
+        }
+    }
+}
+
+/*
+ * using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,9 +130,8 @@ namespace lab_3_variant_8_zadanie_1
             InputDialog inputDialog = new InputDialog();
             if (inputDialog.ShowDialog() == true)
             {
-             //   int[] inputNumbers = inputDialog.Numbers;
-//FillDataGrid(inputNumbers, DataGridView);
-randomNumbers = inputDialog.Numbers;
+     
+                randomNumbers = inputDialog.Numbers;
                 FillDataGrid(randomNumbers, DataGridView);
 
 
@@ -39,6 +149,9 @@ randomNumbers = inputDialog.Numbers;
             }
         }
 
+
+
+         
 
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
@@ -104,8 +217,5 @@ randomNumbers = inputDialog.Numbers;
             return lessThanOne.Concat(greaterThanOne).ToArray();
         }
     }
-
-
-
-
 }
+*/
