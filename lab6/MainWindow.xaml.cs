d1e690b634cbcc25ec6dd67e5041b7684a8aefc1
nikitazapekin <wotblitz362@mail.ru>
2024-10-21 11:06:36 +0300

    first

---
+++ lab6/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using lab6;
+using System;
+using System.Windows;
+
+namespace lab6
+{
+    public partial class MainWindow : Window
+    {
+        private StudentGroup group = new StudentGroup();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            DateTime dateOfBirth = DateOfBirthPicker.SelectedDate ?? DateTime.Now;
+
+            group.AddStudent(new Student(firstName, lastName, dateOfBirth));
+            UpdateResult();
+        }
+
+        private void RemoveStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBox.Text;
+            Student student = group.FindStudentByLastName(lastName);
+            if (student != null)
+            {
+                group.RemoveStudent(student);
+            }
+            UpdateResult();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            group.SortByLastName();
+            UpdateResult();
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBox.Text;
+            Student student = group.FindStudentByLastName(lastName);
+            if (student != null)
+            {
+                ResultTextBlock.Text = $"Найден студент: {student}";
+            }
+            else
+            {
+                ResultTextBlock.Text = "Студент не найден.";
+            }
+        }
+
+        private void UpdateResult()
+        {
+            ResultTextBlock.Text = group.ToString();
+        }
+    }
+}
+
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+*/
---
+++ lab6/Student.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lab6
+{
+
+public class Student : IComparable<Student>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+    public Student(string firstName, string lastName, DateTime dateOfBirth)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        DateOfBirth = dateOfBirth;
+    }
+
+    public int CompareTo(Student other)
+    {
+        return LastName.CompareTo(other.LastName); // Сравнение по фамилии
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}, Дата рождения: {DateOfBirth.ToShortDateString()}";
+    }
+}
+
+public class StudentGroup
+{
+    private List<Student> students = new List<Student>();
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+
+    public void RemoveStudent(Student student)
+    {
+        students.Remove(student);
+    }
+
+    public Student FindStudentByLastName(string lastName)
+    {
+        return students.FirstOrDefault(s => s.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void SortByLastName()
+    {
+        students.Sort();
+    }
+
+    public override string ToString()
+    {
+        return string.Join("\n", students.Select(s => s.ToString()));
+    }
+
+    public Student this[int index]
+    {
+        get { return students[index]; }
+    }
+
+    public int Count => students.Count;
+}
+}
+
+
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    internal class Student
+    {
+    }
+}
+*/
