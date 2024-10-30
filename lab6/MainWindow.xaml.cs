19632273c5fce167175b0f42dd2986d6fd39aad7
nikitazapekin <wotblitz362@mail.ru>
2024-10-30 13:12:22 +0300

    fix validation

--- lab6/MainWindow.xaml.cs
+++ lab6/MainWindow.xaml.cs
@@ -1,4 +1,258 @@
-﻿using lab6;
+﻿
+
+using lab6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab6
+{
+    public partial class MainWindow : Window
+    {
+        private StudentGroup group = new StudentGroup();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string dateFormat = "dd.MM.yy";
+
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                MessageBox.Show("Пожалуйста, введите имя студента!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Пожалуйста, введите фамилию студента!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!DateOfBirthPicker.SelectedDate.HasValue)
+            {
+                MessageBox.Show("Пожалуйста, выберите дату рождения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            DateTime dateOfBirth;
+            if (!DateTime.TryParseExact(DateOfBirthPicker.SelectedDate.Value.ToString(dateFormat), dateFormat, null, System.Globalization.DateTimeStyles.None, out dateOfBirth))
+            {
+                MessageBox.Show($"Некорректный формат даты. Ожидаемый формат: {dateFormat}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            group.AddStudent(new Student(firstName, lastName, dateOfBirth));
+            UpdateResult();
+        }
+
+        private void RemoveStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBoxRemove.Text;
+            var studentsToRemove = group.FindStudentsByLastName(lastName);
+
+            if (studentsToRemove.Count > 0)
+            {
+                foreach (var student in studentsToRemove)
+                {
+                    group.RemoveStudent(student);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Студент для удаления не найден!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            UpdateResult();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (group.Count > 0)
+            {
+                string selectedCriteria = (SortCriteriaComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+                switch (selectedCriteria)
+                {
+                    case "Сортировать по имени":
+                        group.SortByFirstName();
+                        break;
+                    case "Сортировать по фамилии":
+                        group.SortByLastName();
+                        break;
+                    case "Сортировать по дате рождения":
+                        group.SortByDateOfBirth();
+                        break;
+                    default:
+                        MessageBox.Show("Пожалуйста, выберите критерий сортировки.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                }
+
+                UpdateResult();
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно студентов для сортировки!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBox.Text;
+            var foundStudents = group.FindStudentsByLastName(lastName);
+
+            if (foundStudents.Count > 0)
+            {
+                ResultTextBlock.Text = "Найденные студенты:\n" + string.Join("\n", foundStudents.Select(s => s.ToString()));
+            }
+            else
+            {
+                ResultTextBlock.Text = "Студенты не найдены.";
+            }
+        }
+
+        private void UpdateResult()
+        {
+            ResultTextBlockAll.Text = "Студенты: \n" + group.ToString();
+        }
+    }
+}
+
+/*
+ * 
+ * using lab6;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace lab6
+{
+    public partial class MainWindow : Window
+    {
+        private StudentGroup group = new StudentGroup();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            string dateFormat = "dd.MM.yy";
+
+            // Проверка на пустое имя
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                MessageBox.Show("Пожалуйста, введите имя студента!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Проверка на пустую фамилию
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Пожалуйста, введите фамилию студента!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Проверка на пустую или некорректную дату рождения
+            if (!DateOfBirthPicker.SelectedDate.HasValue)
+            {
+                MessageBox.Show("Пожалуйста, выберите дату рождения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Проверка формата даты
+            DateTime dateOfBirth;
+            if (!DateTime.TryParseExact(DateOfBirthPicker.SelectedDate.Value.ToString(dateFormat), dateFormat, null, System.Globalization.DateTimeStyles.None, out dateOfBirth))
+            {
+                MessageBox.Show($"Некорректный формат даты. Ожидаемый формат: {dateFormat}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // Добавление студента после успешной проверки
+            group.AddStudent(new Student(firstName, lastName, dateOfBirth));
+            UpdateResult();
+        }
+
+        private void RemoveStudentButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBoxRemove.Text;
+            Student student = group.FindStudentByLastName(lastName);
+            if (student != null)
+            {
+                group.RemoveStudent(student);
+            }
+            else
+            {
+                MessageBox.Show("Студент для удаления не найден!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            UpdateResult();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (group.Count > 0)
+            {
+                string selectedCriteria = (SortCriteriaComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+                switch (selectedCriteria)
+                {
+                    case "Сортировать по имени":
+                        group.SortByFirstName();
+                        break;
+                    case "Сортировать по фамилии":
+                        group.SortByLastName();
+                        break;
+                    case "Сортировать по дате рождения":
+                        group.SortByDateOfBirth();
+                        break;
+                    default:
+                        MessageBox.Show("Пожалуйста, выберите критерий сортировки.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                }
+
+                UpdateResult();
+            }
+            else
+            {
+                MessageBox.Show("Недостаточно студентов для сортировки!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void FindButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lastName = SearchTextBox.Text;
+            Student student = group.FindStudentByLastName(lastName);
+            if (student != null)
+            {
+                ResultTextBlock.Text = $"Найден студент: {student}";
+            }
+            else
+            {
+                ResultTextBlock.Text = "Студент не найден.";
+            }
+        }
+
+        private void UpdateResult()
+        {
+            ResultTextBlockAll.Text = "Студенты: \n" + group.ToString();
+        }
+    }
+}
+*/
+
+/*
+ * 
+ * using lab6;
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -45,21 +299,7 @@ namespace lab6
             }
             UpdateResult();
         }
-        /*
-        private void SortButton_Click(object sender, RoutedEventArgs e)
-        {
-            //  if(group.Lenb)
-            if (group.Count > 0)
-            {
-
-            group.SortByLastName();
-            UpdateResult();
-            } else
-            {
-                MessageBox.Show("Недостаточно студентов для сортировки по фамилии!");
-            }
-        }
-        */
+ 
 
 
         private void SortButton_Click(object sender, RoutedEventArgs e)
@@ -113,4 +353,4 @@ namespace lab6
         }
     }
 }
- 
+ */
--- lab6/Student.cs
+++ lab6/Student.cs
@@ -1,4 +1,86 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace lab6
+{
+    public class Student : IComparable<Student>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string firstName, string lastName, DateTime dateOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int CompareTo(Student other)
+        {
+            return LastName.CompareTo(other.LastName);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}, Дата рождения: {DateOfBirth.ToShortDateString()}";
+        }
+    }
+
+    public class StudentGroup
+    {
+        private List<Student> students = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void RemoveStudent(Student student)
+        {
+            students.Remove(student);
+        }
+
+        public List<Student> FindStudentsByLastName(string lastNamePart)
+        {
+            return students
+                .Where(s => s.LastName.StartsWith(lastNamePart, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+        }
+
+        public void SortByFirstName()
+        {
+            students = students.OrderBy(s => s.FirstName).ToList();
+        }
+
+        public void SortByLastName()
+        {
+            students = students.OrderBy(s => s.LastName).ToList();
+        }
+
+        public void SortByDateOfBirth()
+        {
+            students = students.OrderBy(s => s.DateOfBirth).ToList();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", students.Select(s => s.ToString()));
+        }
+
+        public Student this[int index]
+        {
+            get { return students[index]; }
+        }
+
+        public int Count => students.Count;
+    }
+}
+
+
+/*
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -68,13 +150,7 @@ public class StudentGroup
             students = students.OrderBy(s => s.DateOfBirth).ToList();
         }
 
-        /*
-
-        public void SortByLastName()
-    {
-        students.Sort();
-    }
-*/
+    
     public  string ToString()
     {
         return string.Join("\n", students.Select(s => s.ToString()));
@@ -89,4 +165,4 @@ public class StudentGroup
 }
 }
 
- 
+ */
