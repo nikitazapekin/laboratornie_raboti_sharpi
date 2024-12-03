be389f6f505007a219d5ffcc64fa8e239e06bca9
nikitazapekin <wotblitz362@mail.ru>
2024-12-03 18:27:12 +0300

    last

---
+++ Lab11/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using Path = System.IO.Path;
+
+namespace Lab11;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private CancellationTokenSource _cancellationTokenSource;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _cancellationTokenSource = new CancellationTokenSource();
+        
+    }
+
+    private async void ProcessTextFile_Click(object sender, RoutedEventArgs e)
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "text_file.txt");
+        if (!File.Exists(filePath))
+        {
+            MessageBox.Show("Файл не найден.");
+            return;
+        }
+        StartBackgroundTask();
+        textBoxInput.Text = "Чтение и обработка текста...";
+        string text = await File.ReadAllTextAsync(filePath);
+
+        string[] words = text.Split(' ');
+        string[] vowels = { "A", "E", "I", "O", "U", "a", "e", "i", "o", "u" };
+        StringBuilder result = new StringBuilder();
+
+        await Task.Run( async () =>
+        {
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (vowels.Contains(words[i][0].ToString()))
+                {
+                    words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+                }
+
+                if (i % 100 == 0) 
+                {
+                    Dispatcher.Invoke(() => { textBoxInput.Text = $"Преобразованный текст:\n{result.ToString()}"; });
+                }
+                
+                result.Append(words[i] + " ");
+            }
+        });
+
+        textBoxInput.Text = "Преобразованный текст:\n" + result.ToString();
+        _cancellationTokenSource.Cancel();
+    }
+
+    private async void StartBackgroundTask()
+    {
+        await Task.Run(() => GenerateFibonacciSequence(_cancellationTokenSource.Token));
+    }
+
+    private void GenerateFibonacciSequence(CancellationToken token)
+    {
+        long first = 0, second = 1;
+        while (!token.IsCancellationRequested)
+        {
+            long next = first + second;
+            first = second;
+            second = next;
+
+            Dispatcher.Invoke(() => { textBoxSecondaryOutput.Text += $"Число Фибоначчи: {next}\n"; });
+
+            Thread.Sleep(500); 
+        }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        _cancellationTokenSource.Cancel();
+    }
+}
