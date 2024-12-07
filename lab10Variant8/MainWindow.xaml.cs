d3155368ed365f34348181031d533d020334cc75
nikitazapekin <wotblitz362@mail.ru>
2024-12-07 12:38:56 +0300

    add panel

--- lab10Variant8/MainWindow.xaml.cs
+++ lab10Variant8/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace lab10Variant8
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            // Заполнение значений для выбора шрифта
+            FontFamilyComboBox.ItemsSource = Fonts.SystemFontFamilies;
+            FontWeightComboBox.ItemsSource = new[] { FontWeights.Normal, FontWeights.Bold, FontWeights.Bold };
+        }
+
+        private void EditGraphMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditPanel.Visibility = Visibility.Visible;
+            GraphEditor.Visibility = Visibility.Visible;
+            TextEditor.Visibility = Visibility.Collapsed;
+        }
+
+        private void EditTextMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditPanel.Visibility = Visibility.Visible;
+            GraphEditor.Visibility = Visibility.Collapsed;
+            TextEditor.Visibility = Visibility.Visible;
         }
     }
 }
